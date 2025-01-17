36b2cdc9e8eae6bc51790b522a0819d137bb50b0
Maicon Gilton <maicongilton@gmail.com>
2025-01-17 14:20:08 -0800

    feat: add router handlers

---
+++ handler/handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetOpening(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Get opening",
+	})
+}
+
+func CreateOpening(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Get opening",
+	})
+}
+
+func EditOpening(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Get opening",
+	})
+}
+
+func DeleteOpening(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Get opening",
+	})
+}
+
+func ListOpenings(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Get opening",
+	})
+}
--- router/routes.go
+++ router/routes.go
@@ -1,7 +1,7 @@
 package router
 
 import (
-	"net/http"
+	"job-openings/handler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,34 +9,10 @@ import (
 func InitRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Get opening",
-			})
-		})
-
-		v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Post opening",
-			})
-		})
-
-		v1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Put opening",
-			})
-		})
-
-		v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Delete opening",
-			})
-		})
-
-		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "List openings",
-			})
-		})
+		v1.GET("/opening", handler.GetOpening)
+		v1.POST("/opening", handler.CreateOpening)
+		v1.PUT("/opening", handler.EditOpening)
+		v1.DELETE("/opening", handler.DeleteOpening)
+		v1.GET("/openings", handler.ListOpenings)
 	}
 }
