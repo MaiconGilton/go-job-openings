d8797e29d5a1871cb9a6f61a9b366480aa114dbe
Maicon Gilton <maicongilton@gmail.com>
2025-01-17 22:09:03 -0800

    feat: setup swagger

--- router/router.go
+++ router/router.go
@@ -1,13 +1,23 @@
 package router
 
 import (
+	"job-openings/docs"
+
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRouter() {
+	docs.SwaggerInfo.Title = "Job Openings API Documentation"
+	docs.SwaggerInfo.Description = "This is a sample server for Job Openings."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "localhost"
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
 	router := gin.Default()
-
 	InitRoutes(router)
-
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Run()
 }
